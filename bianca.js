e494aea03d27d2024e59e14a0dc7ae7dcb1103e3
Biancaamaka <atasie_chiamaka@yahoo.com>
2020-06-03 14:18:37 +0100

    initialize

---
+++ bianca.js
@@ -0,0 +1,4 @@
+const myProfile = () => {
+    return 'Hello World this is Chiamaka Atasie with HNGi7 ID HNG-04060 and email atasie_chiamaka@yahoo.com using JavaScript for stage 2 task';
+};
+console.log(myProfile());
